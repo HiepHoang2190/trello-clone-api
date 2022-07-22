072323502748578cec0dcdecca09eb61030942d6
hieplotus <hieph@lotusoutsourcing.com>
2022-07-22 11:59:07 +0700

    Config babeljs.

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 import express from 'express'
-import { mapOrder } from './utilities/sorts.js'
+import { mapOrder } from '~/utilities/sorts.js'
 
 const app = express()
 
 const hostname = 'localhost'
 const port = 8017
 
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
   res.end('<h1>Hello world!</h1><hr/>')
 })
 
