3205053f4599b66e8d164acdbd45f23fd9cf4391
hieplotus <hieph@lotusoutsourcing.com>
2022-07-22 16:58:04 +0700

    design database 1

--- src/server.js
+++ src/server.js
@@ -1,15 +1,27 @@
 import express from 'express'
-import { connectDB } from '~/config/mongodb'
+import { connectDB, getDB } from '~/config/mongodb'
 import { env } from '~/config/environtment.js'
 
-const app = express()
+connectDB()
+  .then(() => console.log('Connected successfully to database server!'))
+  .then(() => bootServer())
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
 
-connectDB().catch(console.log)
+const bootServer = () => {
+  const app = express()
 
-app.get('/', (req, res) => {
-  res.end('<h1>Hello world!</h1><hr/>')
-})
+  app.get('/', (req, res) => {
+    const dbInsstance = getDB()
 
-app.listen(env.PORT, env.HOST, () => {
-  console.log(`Hello lotusdev, I'm running at ${env.HOST}:${env.PORT}/`)
-})
+    res.end('<h1>Hello world!</h1><hr/>')
+  })
+
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    console.log(`Hello lotusdev, I'm running at ${env.APP_HOST}:${env.APP_PORT}/`)
+  })
+}
+
+
